5920cb4f6610a16f8d77b4796cc378883fe914d5
Your Name <you@example.com>
2024-09-27 14:27:25 +0300

    last commit

--- src/main.py
+++ src/main.py
@@ -3,8 +3,28 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = price
+
+    @classmethod
+    def new_product(cls, my_dict):
+        return cls(
+            my_dict["name"],
+            my_dict["description"],
+            my_dict["price"],
+            my_dict["quantity"],
+        )
 
 
 class Category:
@@ -14,7 +34,52 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.count_category += 1
         Category.count_products += len(products)
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.count_products += 1
+
+    @property
+    def products(self):
+        return [
+            f"{product.name}, {product.price}, Остаток: {product.quantity}"
+            for product in self.__products
+        ]
+
+
+# if __name__ == "__main__":
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     category1 = Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [product1, product2, product3]
+#     )
+#
+#     print(category1.products)
+#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#     category1.add_product(product4)
+#     print(category1.products)
+#     print(category1.count_products)
+#
+#     new_product = Product.new_product(
+#         {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+#          "quantity": 5})
+#     print(new_product.name)
+#     print(new_product.description)
+#     print(new_product.price)
+#     print(new_product.quantity)
+#
+#     new_product.price = 800
+#     print(new_product.price)
+#
+#     new_product.price = -100
+#     print(new_product.price)
+#     new_product.price = 0
+#     print(new_product.price)
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,24 +5,23 @@ from src.main import Category, Product
 
 @pytest.fixture()
 def products():
-    return Product("cola", "вкусная", 500, 6)
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+    return category1
 
 
-@pytest.fixture()
-def category_1():
-    return Category("drinks", "for drink", ["cola", "fanta", "sprite"])
-
-
-@pytest.fixture()
-def category_2():
-    return Category("phones", "mobile phones", ["apple", "samsung", "xiaomi"])
-
-
-def test_init(products, category_1, category_2):
-    assert products.name == "cola"
-    assert products.description == "вкусная"
-    assert category_1.name == "drinks"
-    assert category_1.description == "for drink"
-    assert category_1.products == ["cola", "fanta", "sprite"]
-    assert Category.count_products == 6
-    assert Category.count_category == 2
+def test_init(products):
+    assert products.products == [
+        "Samsung Galaxy S23 Ultra, 180000.0, Остаток: 5",
+        "Iphone 15, 210000.0, Остаток: 8",
+        "Xiaomi Redmi Note 11, 31000.0, Остаток: 14",
+    ]
