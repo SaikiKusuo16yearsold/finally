dc2b8738014f80de84343a57b755d6a3c07a5706
Your Name <you@example.com>
2024-10-03 18:37:27 +0300

    last commit

--- src/main.py
+++ src/main.py
@@ -3,8 +3,14 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.__price = price
+        self.price = price
         self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт"
+
+    def __add__(self, other):
+        return self.price * self.quantity + other.__price * other.quantity
 
     @property
     def price(self):
@@ -39,6 +45,12 @@ class Category:
         Category.count_category += 1
         Category.count_products += len(products)
 
+    def __str__(self):
+        counter = 0
+        for i in self.__products:
+            counter = counter + i.quantity
+        return f"{self.name} {counter} шт"
+
     def add_product(self, product: Product):
         self.__products.append(product)
         Category.count_products += 1
@@ -51,35 +63,25 @@ class Category:
         ]
 
 
-# if __name__ == "__main__":
+# if __name__ == 'main':
 #     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 #     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 #     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+# print(str(product1))
+#     print(str(product2))
+#     print(str(product3))
 #
 #     category1 = Category(
 #         "Смартфоны",
 #         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
 #         [product1, product2, product3]
 #     )
-#
+#     #
+#     print(str(category1))
+#     #
 #     print(category1.products)
-#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-#     category1.add_product(product4)
-#     print(category1.products)
-#     print(category1.count_products)
 #
-#     new_product = Product.new_product(
-#         {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-#          "quantity": 5})
-#     print(new_product.name)
-#     print(new_product.description)
-#     print(new_product.price)
-#     print(new_product.quantity)
-#
-#     new_product.price = 800
-#     print(new_product.price)
-#
-#     new_product.price = -100
-#     print(new_product.price)
-#     new_product.price = 0
-#     print(new_product.price)
+#     print(product1 + product2)
+#     print(product1 + product3)
+#     print(product2 + product3)
--- tests/test_main.py
+++ tests/test_main.py
@@ -19,9 +19,48 @@ def products():
     return category1
 
 
-def test_init(products):
+@pytest.fixture()
+def answer1():
+    product = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+
+    return product
+
+
+@pytest.fixture()
+def answer():
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    return str(category1)
+
+
+@pytest.fixture()
+def answer2():
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+    return product1 + product2
+
+
+def test_init(products, answer, answer1, answer2):
     assert products.products == [
         "Samsung Galaxy S23 Ultra, 180000.0, Остаток: 5",
         "Iphone 15, 210000.0, Остаток: 8",
         "Xiaomi Redmi Note 11, 31000.0, Остаток: 14",
     ]
+    assert str(answer1) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт"
+    assert answer == "Смартфоны 27 шт"
+    assert answer2 == 2580000.0
